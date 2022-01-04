1a7203b7b8e87f570a00ad0266bc938cd0005d64
Dume Momoh <dumeccs@gmail.com>
2022-01-04 14:39:00 +0100

    react state initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const style = {width:'500px',height:'400px'}
+class App extends React.Component{
+
+  state = {
+  showState: true,
+   person:{
+    fullName : 'Brother Bear',
+    bio: "Brother's keeper",
+    imgSrc:'./Bkeeper.jpg',
+    profession:'care taker'
+   }
+  }
+ 
+
+  render(){
+    return (
+      
+      <div className="App">
+       <h2>Profile</h2>
+       <img src = {this.state.person.imgSrc} alt="users-profile" style = {style} />
+       <p> Fullname : {this.state.person.fullName}</p>
+       <p>Bio: {this.state.person.bio}</p>
+       <p>Profession: {this.state.person.profession}</p>
+
+       <button onClick={this.clickFunc}>Show Profile</button>
+      </div>
+      
+    );
+  }
+
+  
+  
 }
 
 export default App;
